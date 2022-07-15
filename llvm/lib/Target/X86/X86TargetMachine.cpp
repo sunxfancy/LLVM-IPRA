8e11dc7eac7859ce7b0550c196d005e7e24b8eac
Xiaofan Sun <sunxfancy@gmail.com>
2022-07-15 23:02:06 +0000

    move output to a file

--- llvm/lib/Target/X86/X86TargetMachine.cpp
+++ llvm/lib/Target/X86/X86TargetMachine.cpp
@@ -531,7 +531,6 @@ void X86PassConfig::addPostRegAlloc() {
 
 void X86PassConfig::addPreSched2() { 
   addPass(createX86ExpandPseudoPass()); 
-  addPass(createCountPushPopPass());
 }
 
 void X86PassConfig::addPreEmitPass() {
@@ -594,6 +593,9 @@ void X86PassConfig::addPreEmitPass2() {
 
   // Insert pseudo probe annotation for callsite profiling
   addPass(createPseudoProbeInserter());
+
+  // Add count push pop pass for study the register savings
+  addPass(createCountPushPopPass());
 
   // On Darwin platforms, BLR_RVMARKER pseudo instructions are lowered to
   // bundles.
